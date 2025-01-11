f469749d7062ec82552e2bc1a693d264406d4b22
Shiba Sundar Behera <shibasundarbehera@gmail.com>
2025-01-11 10:00:00 +0530

    changes commit as Shiba

---
+++ react-project/src/components/Post.jsx
@@ -0,0 +1,10 @@
+let Post =()=>{
+  return (
+    <div className="post">
+      <h2>Post Title</h2>
+      <p>This is a sample post content.by Shiba ..</p>
+    </div>
+  );
+}
+
+export default Post;
