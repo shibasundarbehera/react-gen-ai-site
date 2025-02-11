62f770ced2e6ffe0636297a987bf700aed4a6c4c
Shiba Sundar Behera <shibasundarbehera@gmail.com>
2025-02-11 10:00:00 +0530

    changes commit as Shiba

--- react-project/src/assets/scripts/app.js
+++ react-project/src/assets/scripts/app.js
@@ -6,3 +6,32 @@
 
 console.log('app.js loaded');
 console.log (3 + 4);
+
+class App {
+  constructor() {
+    this.name = 'App';
+    this.version = '1.0.0';
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getVersion() {
+    return this.version;
+  }
+}
+
+let app = new App();
+console.log(`App Name: ${app.getName()}`);
+console.log(`App Version: ${app.getVersion()}`);
+
+const hobbies = ['Reading', 'Gaming', 'Hiking', 'Cooking'];
+ 
+const index = hobbies.findIndex((hobby) => hobby === 'Gaming') !== -1 
+console.log(`Index of 'Gaming': ${index}`);
+
+hobbies.map((hobby) => {
+  console.log(`Hobby: ${hobby}`);
+}
+);
