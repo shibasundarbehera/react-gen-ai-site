925adaef5654e5f9a46ffea89a1b1cee8b68fb6e
Shiba Sundar Behera <shibasundarbehera@gmail.com>
2025-02-24 10:00:00 +0530

    changes commit as Shiba

--- react-project/src/components/Post.jsx
+++ react-project/src/components/Post.jsx
@@ -6,7 +6,7 @@ let Post =(props)=>{
     let author  = props.author || chosenName;
     let body = props.body || "This is a default post content.";
   return (
-    <div className="post">
+    <div className="post" style = {{border: '1px solid #ccc', padding: '10px', margin: '10px 0', color: '#333'}}>
       <h2>Post Title</h2>
       <p>Author: { author }</p>
       <p>Date: {new Date().toLocaleDateString()}</p>
