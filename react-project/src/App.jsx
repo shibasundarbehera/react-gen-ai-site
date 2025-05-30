360ab005c6be8e486b3a534f62585f56196037d1
Shiba Sundar Behera <ssbehera@processmap.com>
2025-05-30 12:29:37 +0530

    minor changes

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -5,35 +5,43 @@ import './App.css'
 import Post from './components/Post'
 
 function App() {
-  const [count, setCount] = useState(0)
+ // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Post />
+    // <>
+    //   <div>
+    //     <a href="https://vite.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    //   <Post />
+    //   <div className="footer">
+    //     <p>Made with ❤️ by Shiba</p>
+    //   </div>
+    // </>
+    <main>
+      <Post author = "ssb" body ="react is awesome" />
+      <Post author = "ssb2" body ="react is awesome really " /> 
+      <Post /> 
       <div className="footer">
         <p>Made with ❤️ by Shiba</p>
       </div>
-    </>
+    </main>
 
   )
 }
--- react-project/src/components/Post.jsx
+++ react-project/src/components/Post.jsx
@@ -1,13 +1,16 @@
 const names =['Shiba', 'Luna', 'Max', 'Bella', 'Charlie'];
 
-let Post =()=>{
+ 
+let Post =(props)=>{
     const chosenName = names[Math.floor(Math.random() * names.length)];
+    let author  = props.author || chosenName;
+    let body = props.body || "This is a default post content.";
   return (
     <div className="post">
       <h2>Post Title</h2>
-      <p>Author: {chosenName}</p>
+      <p>Author: { author }</p>
       <p>Date: {new Date().toLocaleDateString()}</p>
-      <p>This is a sample post content.by Shiba ..</p>
+      <p> {body}</p>
     </div>
   );
 }
